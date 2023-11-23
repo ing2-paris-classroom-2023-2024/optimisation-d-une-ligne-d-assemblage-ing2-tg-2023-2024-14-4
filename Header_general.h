0aa09f635929899e681970082752c2a90859ee92
Alexandre Celles <alexandre.celles@edu.ece.fr>
2023-11-23 18:32:22 +0100

    Mofication et Structure Station

--- Header_general.h
+++ Header_general.h
@@ -11,28 +11,29 @@
 
 typedef struct station
 {
-    int station;
-    float tempstotal;
+    int* station;          // Liste de toutes les actions présentes dans la station
+    float tempstotal;      // Temps total de chaque station (Addition de toutes les actions à l'intérieur de la station)
 }t_station;
 
-typedef struct actions_duree
+typedef struct actions
 {
-    int num_action;
+    int num_action;           // Numéro de l'action
     int disponible;           //est ce que l action est deja dans l assemblage ou pas
     float temps_action;
-    int precedence;
+    int *precedence;
     int nombre_de_precedence;
-    int exclusion;
-    int nombre_de_exclusion;
+    int *exclusion;           // Liste de toutes les exclusions
+    int nombre_de_exclusion;  // Nombre d'exclusions pour l'action
 }t_actions;
 
 typedef struct assemblage
 {
-    t_station tab_station;
-    t_actions *tab_actions;
+    t_station *tab_station;           // Tableau de toutes les stations
+    t_actions *tab_actions;     // Tableau de toutes les actions
     int nombre_actions;
     int nombre_stations;
 }t_assemblage;
 
 void recuperation_de_donnees(t_assemblage *voiture);
+int indice(int valeurdelaction,t_assemblage voiture);
 #endif //PROJETS1_HEADER_GENERAL_H
