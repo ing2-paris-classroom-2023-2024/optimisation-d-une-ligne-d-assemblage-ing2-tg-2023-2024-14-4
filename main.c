75bd2050c850971af84c6f64cd87905609b4c205
Timothee Petit <timfrteam0@gmail.com>
2023-11-26 17:30:02 +0100

    test giteub

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main()
     recuperation_de_donnees(&voiture);
     //fonction_generale(&voiture);
     mainprecedence();
-    // testprecedence(); //fonction de test
+    // testprecedence();
 
 
     return 0;
