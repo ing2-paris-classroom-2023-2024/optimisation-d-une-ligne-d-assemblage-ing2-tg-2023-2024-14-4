c9496e9837487171eb795c3ac04581f4ddec72e5
Timothee Petit <timfrteam0@gmail.com>
2023-12-04 09:44:18 +0100

    Ajout de la partie de Timothee

--- main.c
+++ main.c
@@ -12,7 +12,223 @@ int main()
     printf("programme d'exclusion et de precedence :\n\n");
     mainexplusprecedence();
 
+    /*
+    s-In
+     trod
+     uction
+     : Globa
+     l commons
+     ref
+     er to
+     shared
+     resou
+     rces
+     that ar
+     e not
+     owned
+     by any
+     single
+     co
+     untry
+     but
+     are c
+     rucial
+     for the
+     well-being
+     of huma
+     nity. E
+     xamples
+     include
+     the atmos
+     phere, oce
+     ans, an
+     d o
+     uter
+     space.
+     These r
+     esources
+     require a
+     framework
+     or gove
+     rnanc
+     e strate
+     gy due t
+     o the p
+     otential
+     for over
+     use, mis
+     use,
+     or degr
+     adatio
+     n. In th
+     is report,
+     we explor
+     e t
+     he c
+     h
+     allen
+     ges an
+     d possib
+     le gove
+     rnance fr
+     ameworks
+     for ma
+     nag
+     ing a
+     spe
+     cific
+     glob
+     al co
+     mmons i
+     ssue: spa
+     ce de
+     bri
+     s.
+     The th
+     esis is
+     that effe
+     ctive
+     collec
+     tive go
+     vernan
+     ce
+     is e
+     ssen
+     tial
+     to add
+     ress t
+     he grow
+     ing thre
+     at of
+     s
+     pac
+     e d
+     ebris
+     and e
+     nsure
+     sustai
+     nabl
+     e
+     use of
+     outer s
+     pace
 
+   Glo
+     bal
+     Commo
+     ns, G
+     loba
+     l P
+     ublic
+     Goods,
+     Pri
+     vate Go
+     ods
+     (a
+
+     pproxi
+     mately
+     250 wor
+     ds):
+     Spa
+     ce
+     debri
+     s, con
+     sisting
+     of
+     def
+     unct
+     satel
+     lites
+     and f
+     rag
+     ments
+     from pa
+     st sp
+     ac
+     e mi
+     ss
+     ion
+     s, po
+     ses a
+     glo
+     bal t
+     hreat a
+     s
+     it
+     orbi
+     ts
+     Earth
+     unc
+     ontr
+     ollab
+     ly.
+     It
+     fal
+     ls u
+     nder
+     the
+     cat
+     egor
+     y of
+     a glo
+     bal c
+     omm
+     on, a
+     s it
+     affec
+     ts a
+     ll
+     natio
+     ns and
+     h
+     as c
+     onse
+     quences
+     for
+     th
+     e en
+     tire g
+     loba
+     l c
+     omm
+     unity
+     . Unlik
+     e pr
+     iv
+     ate go
+     ods, w
+     hic
+     h ca
+     n be o
+     wned a
+     nd
+     cont
+     rolled
+     , sp
+     ace d
+     ebri
+     s re
+     quires
+     inter
+     nat
+     ional
+     coope
+     ra
+     tio
+     n to ad
+     dress
+     i
+     ts
+     chall
+     enges
+     co
+     llec
+     tiv
+     ely.
+
+
+    */
 
     return 0;
 }
